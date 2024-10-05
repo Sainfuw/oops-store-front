5fe19de3feea21d0dbe14f168c4e0bdb420af4c8
Emilio Navarro <sainfuw@outlook.com>
2024-10-05 19:30:12 -0300

    RWK: intefaces

--- src/intefaces/categories.ts
+++ src/intefaces/categories.ts
@@ -1,17 +1,16 @@
-export interface ICategoryStrapi {
+interface ICategoryBase {
   id: number
   name: string
   slug: string
   description: string
+}
+
+export interface ICategoryStrapi extends ICategoryBase {
   image: {
     url: string
   }
 }
 
-export interface ICategory {
-  id: number
-  name: string
-  slug: string
-  description: string
+export interface ICategory extends ICategoryBase {
   image: string
 }
